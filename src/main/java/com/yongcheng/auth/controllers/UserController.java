8a2f5dec9789fdbdca4a84f80f8a1122b5c36218
yongchengzhu <yongchengzhu002@gmail.com>
2020-05-14 22:06:29 -0400

    enable password encoding, but still needs websecurity configured inorder to access routes.

--- src/main/java/com/yongcheng/auth/controllers/UserController.java
+++ src/main/java/com/yongcheng/auth/controllers/UserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
   
   @Autowired
-  UserService userService;
+  private UserService userService;
 
   @PostMapping("/signup")
   public ResponseEntity<?> signup(@RequestBody @Valid SignupRequest signupRequest) {
--- src/main/java/com/yongcheng/auth/sevices/UserService.java
+++ src/main/java/com/yongcheng/auth/sevices/UserService.java
@@ -4,15 +4,20 @@ import com.yongcheng.auth.models.User;
 import com.yongcheng.auth.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
   @Autowired
-  UserRepository userRepository;
+  private UserRepository userRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   public void save(User user) {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
     userRepository.save(user);
   }
 }
