33bb7d8fa2eeaa8bb21c40644da106e4808972f8
yongchengzhu <yongchengzhu002@gmail.com>
2020-05-18 15:02:03 -0400

    send confirmation email to registered user using gmail smtp.

--- src/main/java/com/yongcheng/auth/events/OnRegistrationCompleteListener.java
+++ src/main/java/com/yongcheng/auth/events/OnRegistrationCompleteListener.java
@@ -1,5 +1,6 @@
 package com.yongcheng.auth.events;
 
+import com.yongcheng.auth.models.User;
 import com.yongcheng.auth.models.VerificationToken;
 import com.yongcheng.auth.sevices.VerificationTokenService;
 
@@ -7,6 +8,8 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,13 +18,28 @@ public class OnRegistrationCompleteListener implements ApplicationListener<OnReg
   @Autowired
   private VerificationTokenService verificationTokenService;
 
+  @Autowired
+  private JavaMailSender mailSender;
+
 	@Override
 	public void onApplicationEvent(OnRegistrationCompleteEvent event) {
     // 1. Generate a UUID as verification token.
     // 2. Send verification email to user's email.
     String token = UUID.randomUUID().toString();
-    VerificationToken verificationToken = new VerificationToken(token, event.getUser());
+    User user    = event.getUser();
+    VerificationToken verificationToken = new VerificationToken(token, user);
     
     verificationTokenService.save(verificationToken);
+
+    SimpleMailMessage email = new SimpleMailMessage();
+    email.setTo(user.getEmail());
+    email.setSubject("Registration Confirmation");
+    email.setText(
+      "http://localhost:8080" + 
+      "/api/user/confirm?token" +
+      token
+    );
+
+    mailSender.send(email);
 	}
 }
