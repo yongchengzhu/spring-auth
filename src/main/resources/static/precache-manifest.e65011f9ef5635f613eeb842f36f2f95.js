1353eee38e6a19d83bb6c3bada5b92466c3af020
yongchengzhu <yongchengzhu002@gmail.com>
2020-05-19 13:20:13 -0400

    build and serve react static resources.

---
+++ src/main/resources/static/precache-manifest.e65011f9ef5635f613eeb842f36f2f95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1f4cdfecd43ae0ba856b18558b64ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a355a0c4e91da5b16ce4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecb735992b507d5c0db2",
+    "url": "/static/js/2.00c3760c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00c3760c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a355a0c4e91da5b16ce4",
+    "url": "/static/js/main.67f92295.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
