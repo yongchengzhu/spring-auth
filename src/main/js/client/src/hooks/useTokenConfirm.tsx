002f5a901d27c02611d64d2ea2bb32669be33341
yongchengzhu <yongchengzhu002@gmail.com>
2020-05-23 23:23:14 -0400

    add maven profiles and fix confirm page.

--- src/main/js/client/src/hooks/useTokenConfirm.tsx
+++ src/main/js/client/src/hooks/useTokenConfirm.tsx
@@ -2,18 +2,22 @@ import { useEffect, useState } from "react";
 
 import server from '../apis/server';
 import { RouteComponentProps } from "react-router";
+import { AxiosError } from "axios";
 
 const useTokenConfirm = (props: RouteComponentProps) => {
   const [message, setMessage] = useState('Confirming token, please wait...');
 
   useEffect(() => {
-    const regex: RegExp = /\?token=(?<token>.*)[&?]/;
+    const regex: RegExp = /\?token=(?<token>[^&]*)/;
     const match: RegExpMatchArray | null = props.location.search.match(regex);
     if(match && match.groups) {
       const token:string = match.groups.token;
       server.put(`/user/confirm?token=${token}`)
         .then(() => setMessage('Email confirmation successful, your account is enabled.'))
-        .catch((err: Error) => setMessage(err.message));
+        .catch((err: AxiosError) => err.response
+          ? setMessage(err.response.data.message)
+          : setMessage('Request failed with no response.')
+        );
     } else {
       console.log('Failed: Cannot extract valid token.');
     }
